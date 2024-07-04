37019f67eba70710879df2b66c817c1fe633e930
Eder Silveira <jukika_eder@hotmail.com>
2024-07-04 18:39:48 -0300

    Documentando a classe ContaBancaria

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -1,24 +1,24 @@
 public class ContaBancaria {
 
-    private double saldo;
+    private double saldo; // Armazena o saldo da conta
 
     public ContaBancaria(double saldoInicial) {
-        this.saldo = saldoInicial;
+        this.saldo = saldoInicial; // Inicia a conta com um saldo inicial
     }
 
     public double getSaldo() {
-        return saldo;
+        return saldo;  // Retorna o saldo inicial da conta
     }
 
     public void depositar(double quantia) {
         if (quantia > 0) {
             saldo += quantia;
-        }
+        } // Adiciona uma quantia ao saldo, desde que a quantia seja positiva
     }
 
     public void sacar(double quantia) {
         if (quantia > 0 && quantia <= saldo) {
             saldo -= quantia;
-        }
+        } // Subtrai uma quantia do saldo, desde que a quantia seja positiva e não exceda o saldo disponível
     }
 }
